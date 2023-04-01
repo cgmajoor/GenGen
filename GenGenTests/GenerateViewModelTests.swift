825ce0b5e5a84763de725f14faaf4ba016337f19
CGMajoor <cgmajoor@gmail.com>
2023-04-01 15:37:33 +0200

    add makeSut with TestConditions

--- GenGenTests/GenerateViewModelTests.swift
+++ GenGenTests/GenerateViewModelTests.swift
@@ -12,8 +12,7 @@ import XCTest
 final class GenerateViewModelTests: XCTestCase {
 
     func test_GenerateViewModel_withNoRules_generationIsEmpty() throws {
-        let noRules = [Rule]()
-        let sut = GenerateViewModel(rules: noRules)
+        let sut = makeSut(with: .noRules)
 
         sut.generate()
 
@@ -21,25 +20,62 @@ final class GenerateViewModelTests: XCTestCase {
     }
 
     func test_GenerateViewModel_with1ActiveRuleWithEmptyCategory_generationIsEmpty() throws {
-        let emptyCategory = Category(name: "color", words: [])
-        let activeRuleWithEmptyCategory = Rule(active: true, categories: [emptyCategory])
-        let rules = [activeRuleWithEmptyCategory]
-        let sut = GenerateViewModel(rules: rules)
+        let sut = makeSut(with: .oneActiveRuleWithEmptyCategory)
 
         sut.generate()
 
         XCTAssertEqual(sut.generatedStr, "")
     }
 
-    func test_GenerateViewModel_with1ActiveRuleWith1Possibility_generationIsCorrect() throws {
-        let oneWord = ["pink"]
-        let categoryWith1Word = Category(name: "colors", words: oneWord)
-        let ruleWith1Category = Rule(active: true, categories: [categoryWith1Word])
-        let rules = [ruleWith1Category]
-        let sut = GenerateViewModel(rules: rules)
+    func test_GenerateViewModel_with1ActiveRuleWith1PossibleOutcome_generationIsCorrect() throws {
+        let sut = makeSut(with: .oneActiveRuleWith1PossibleOutcome)
 
         sut.generate()
 
         XCTAssertEqual(sut.generatedStr, "pink")
+    }
+
+    func test_GenerateViewModel_with1InactiveRule_generationIsEmpty() throws {
+        let sut = makeSut(with: .oneInactiveRule)
+
+        sut.generate()
+
+        XCTAssertEqual(sut.generatedStr, "")
+    }
+
+    // MARK: - Test Conditions
+    enum TestCondition {
+        case noRules
+        case oneActiveRuleWithEmptyCategory
+        case oneActiveRuleWith1PossibleOutcome
+        case oneInactiveRule
+    }
+
+    // MARK: - Make System Under Test
+    private func makeSut(with testCondition: TestCondition) -> GenerateViewModel {
+        var sut: GenerateViewModel
+        switch testCondition {
+        case .noRules:
+            let noRules = [Rule]()
+            sut = GenerateViewModel(rules: noRules)
+        case .oneActiveRuleWithEmptyCategory:
+            let emptyCategory = Category(name: "color", words: [])
+            let activeRuleWithEmptyCategory = Rule(active: true, categories: [emptyCategory])
+            let rules = [activeRuleWithEmptyCategory]
+            sut = GenerateViewModel(rules: rules)
+        case .oneActiveRuleWith1PossibleOutcome:
+            let oneWord = ["pink"]
+            let categoryWith1Word = Category(name: "colors", words: oneWord)
+            let ruleWith1Category = Rule(active: true, categories: [categoryWith1Word])
+            let rules = [ruleWith1Category]
+            sut = GenerateViewModel(rules: rules)
+        case .oneInactiveRule:
+            let oneWord = ["pink"]
+            let categoryWith1Word = Category(name: "colors", words: oneWord)
+            let inactiveRule = Rule(active: false, categories: [categoryWith1Word])
+            let rules = [inactiveRule]
+            sut = GenerateViewModel(rules: rules)
+        }
+        return sut
     }
 }
