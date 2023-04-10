2bb8f5a1da0017d96e994c23b5a5f7d42b5e65ea
CGMajoor <cgmajoor@gmail.com>
2023-04-10 18:13:56 +0200

    add tableview to rule creator

--- GenGen/Modules/RuleCreator/RuleCreatorViewController.swift
+++ GenGen/Modules/RuleCreator/RuleCreatorViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class RuleCreatorViewController: UIViewController {
+
+    // MARK: - Properties
+    var selectedBooks: [Book] = []
+
     // MARK: - UI
     lazy var headerLabel = GGLabel(textColor: AppTheme.Navigation.Color.rules,
                                    font:AppTheme.Navigation.FontStyle.title,
@@ -20,6 +24,14 @@ class RuleCreatorViewController: UIViewController {
                                             action: #selector(doneTapped))
         barButtonItem.tintColor = AppTheme.Main.Color.buttonBackground
         return barButtonItem
+    }()
+
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .red
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
     }()
 
     lazy var addButton: GGButton = {
@@ -44,16 +56,25 @@ class RuleCreatorViewController: UIViewController {
     // MARK: - Setup
     private func setup() {
         view.backgroundColor = AppTheme.Main.Color.background
+        tableView.register(LibraryTableViewCell.self, forCellReuseIdentifier: Texts.libraryTableViewCell)
+        tableView.dataSource = self
+
+        view.addSubview(tableView)
         view.addSubview(addButton)
 
         let horizontalPadding = AppTheme.Main.Padding.horizontal
         let verticalPadding = AppTheme.Main.Padding.horizontal
 
         NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: verticalPadding),
+            tableView.heightAnchor.constraint(equalToConstant: (UIScreen.main.bounds.height / 3.0)),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalPadding),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -horizontalPadding),
+
+            addButton.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: verticalPadding),
             addButton.heightAnchor.constraint(equalToConstant: AppTheme.Main.Size.buttonHeight),
             addButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalPadding),
             addButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -horizontalPadding),
-            addButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -verticalPadding)
         ])
     }
 
@@ -64,5 +85,22 @@ class RuleCreatorViewController: UIViewController {
 
     @objc private func addTapped() {
         print("add tapped")
+    }
+}
+
+
+// MARK: - UITableViewDataSource
+extension RuleCreatorViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return selectedBooks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Texts.libraryTableViewCell, for: indexPath) as? LibraryTableViewCell,
+              let bookName = selectedBooks[indexPath.row].name else {
+            return UITableViewCell()
+        }
+        cell.configure(bookName: bookName)
+        return cell
     }
 }
